642f241b2af1e22f0c6863756c42dd1750dae2a1
aivaritobot <walletalvaro@gmail.com>
2026-04-02 01:03:09 -0400

    Refactor RouletteEye to VisionEngine class

--- vision.py
+++ vision.py
@@ -1,29 +1,33 @@
 import cv2
 import numpy as np
-from mss import mss
+import mss
 
-class RouletteEye:
-    def __init__(self, x, y, w, h):
-        # Define el área de la pantalla que el bot va a observar
-        self.monitor = {"top": y, "left": x, "width": w, "height": h}
-        self.sct = mss()
-
-    def get_ball(self):
-        # Toma una captura de pantalla del área de la ruleta
+class VisionEngine:
+    def __init__(self, monitor_area):
+        self.sct = mss.mss()
+        self.monitor = monitor_area # {'top': 100, 'left': 100, 'width': 500, 'height': 500}
+        
+    def get_positions(self):
         img = np.array(self.sct.grab(self.monitor))
         frame = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+        
+        # Tracking de la bola (Blanco puro)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        ball_mask = cv2.inRange(hsv, (0, 0, 220), (180, 40, 255))
         
-        # Filtro para detectar la bola blanca
-        lower = np.array([0, 0, 200])
-        upper = np.array([180, 50, 255])
-        mask = cv2.inRange(hsv, lower, upper)
+        # Tracking del Cero (Verde neón del rotor)
+        zero_mask = cv2.inRange(hsv, (40, 100, 100), (80, 255, 255))
         
+        # Extraer coordenadas (X, Y)
+        ball_coords = self._get_center(ball_mask)
+        zero_coords = self._get_center(zero_mask)
+        
+        return ball_coords, zero_coords, frame
+
+    def _get_center(self, mask):
         cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if cnts:
-            c = max(cnts, key=cv2.contourArea)
-            M = cv2.moments(c)
+            M = cv2.moments(max(cnts, key=cv2.contourArea))
             if M["m00"] != 0:
-                # Retorna las coordenadas X y Y de la bola
-                return int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])
+                return (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         return None
